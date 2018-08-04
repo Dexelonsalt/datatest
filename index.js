9e7401bdb619257733adf7e3f7afe314ba26c558
TheGreenCat <mineharderytps801@gmail.com>
2018-08-04 21:29:57 +0100

    owo

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const Enmap = require("enmap")
-const Lvl = require("enmap-level")
+const EnmapLevel = require("enmap-level")
 
-const provider = new Lvl.EnmapLevel({ name: "test" })
+const provider = new EnmapLevel({ name: "plixacmds", persistent: true })
 const myColl = new Enmap({ provider: provider })
 
-myColl.set('boolean', true);
-myColl.set('integer', 42);
-myColl.set('null', null);
+myColl.set('boolean', 'true');
+myColl.set('integer', '42');
+myColl.set('null', 'nope');
 
 console.log(myColl.get('null'))
