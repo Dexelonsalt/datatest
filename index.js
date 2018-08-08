2a95e7a097977db80f3a10ea4aa158eaab769ec3
TheGreenCat <mineharderytps801@gmail.com>
2018-08-08 13:09:23 +0100

    owo

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const Enmap = require("enmap")
 const EnmapLevel = require("enmap-level")
+const fs = require("fs")
 
 const provider = new EnmapLevel({ name: "plixacmds", persistent: true })
 const myColl = new Enmap({ provider: provider })
 
-console.log(myColl.get('null'))
+fs.writeFile("./data.json", '{ "tests": { "test": "hi" } }', (err) => {
+    console.log(err)
+    console.log(require("./data.json").tests.test)
+})
